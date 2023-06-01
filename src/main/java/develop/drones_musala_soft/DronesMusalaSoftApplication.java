2e22ae67ad73369c280fd766154847c5dbf9a468
rbarrientos@soaint.com <rbarrientos@soaint.com>
2023-05-31 22:48:51 -0400

    Initial commit

---
+++ src/main/java/develop/drones_musala_soft/DronesMusalaSoftApplication.java
@@ -0,0 +1,13 @@
+package develop.drones_musala_soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesMusalaSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronesMusalaSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/develop/drones_musala_soft/DronesMusalaSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package develop.drones_musala_soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesMusalaSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
